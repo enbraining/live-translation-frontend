49ea361da33446eaa66120123073882151f0db3f
Donghak Kim <enbraining@gmail.com>
2025-10-30 16:15:41 +0900

    update cors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["100.83.90.29"],
+  allowedDevOrigins: ["100.83.90.29", "223.130.133.140"],
 };
 
 export default nextConfig;
